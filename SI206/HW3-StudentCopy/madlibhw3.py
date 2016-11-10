c9ad4578aa70c6eada025cf22ab3783bea4ff85e
Robert Bracci <robertbracci@Roberts-MacBook-Pro.local>
2016-11-10 15:05:46 -0500

    updated madlib

--- SI206/HW3-StudentCopy/madlibhw3.py
+++ SI206/HW3-StudentCopy/madlibhw3.py
@@ -5,7 +5,7 @@
 # 1) Only use the first 150 tokens
 # 2) Pick 5 parts of speech to prompt for, including nouns
 # 3) Replace nouns 15% of the time, everything else 10%
-
+##
 # Deliverables:
 # 1) Print the orginal text (150 tokens)
 # 1) Print the new text
